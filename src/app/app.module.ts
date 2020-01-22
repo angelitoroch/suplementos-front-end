10b4901ef2bd4d062a9a4689e7556a8fae6d5adb
Luis Angel Aboytes Rocha <luis.aboytes@hopers.com>
2020-01-22 17:50:19 -0600

    se realizo el diseño del menu posterior de la pagina y se acomodo el contenido y el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { EncabezadoComponent } from './modules-app/encabezado/encabezado.component';
+import { PieComponent } from './modules-app/pie/pie.component';
+import { HomeComponent } from './modules-app/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EncabezadoComponent, PieComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
